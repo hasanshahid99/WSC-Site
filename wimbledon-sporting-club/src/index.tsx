90e100c5e30400b6195681079860d74ed2722137
hasanshahid99 <shahidhasan148@gmail.com>
2023-11-02 11:51:55 +0000

    eslint error

--- wimbledon-sporting-club/src/index.tsx
+++ wimbledon-sporting-club/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router} from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
